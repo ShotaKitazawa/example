c4530eabf8fc24f874b027fea6de0c2426dd2924
ShotaKitazawa <skitazawa1121@gmail.com>
2020-09-17 15:19:18 +0900

    update

--- setup.go
+++ setup.go
@@ -14,13 +14,13 @@ func init() { plugin.Register("example", setup) }
 // setup is the function that gets called when the config parser see the token "example". Setup is responsible
 // for parsing any extra options the example plugin may have. The first token this function sees is "example".
 func setup(c *caddy.Controller) error {
-	//c.Next() // Ignore "example" and give us the next token.
-	//if c.NextArg() {
-	//	// If there was another token, return an error, because we don't have any configuration.
-	//	// Any errors returned from this setup function should be wrapped with plugin.Error, so we
-	//	// can present a slightly nicer error message to the user.
-	//	return plugin.Error("example", c.ArgErr())
-	//}
+	c.Next() // Ignore "example" and give us the next token.
+	if c.NextArg() {
+		// If there was another token, return an error, because we don't have any configuration.
+		// Any errors returned from this setup function should be wrapped with plugin.Error, so we
+		// can present a slightly nicer error message to the user.
+		return plugin.Error("example", c.ArgErr())
+	}
 
 	// Add a startup function that will -- after all plugins have been loaded -- check if the
 	// prometheus plugin has been used - if so we will export metrics. We can only register
